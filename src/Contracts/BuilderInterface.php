a814ee328eb1d96fb4c8007602ef16966ceaff24
KietNT <113796420+TanNhatCMS@users.noreply.github.com>
2025-11-18 21:21:19 +0700

    Initial commit: add Laravel caching model package
    Add base files for a Laravel package providing Eloquent model caching, including interfaces, trait, query builder, exception, and documentation. Includes .editorconfig, .gitattributes, .gitignore, composer.json, LICENSE, and README.md.

---
+++ src/Contracts/BuilderInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace KietNT\CachingModel\Contracts;
+
+use Closure;
+use Illuminate\Support\Collection;
+
+interface BuilderInterface
+{
+    public function find($id);
+
+    public function findByKey($key, $value);
+
+    public function setCacheKey(string $key): BuilderInterface;
+
+    public function get($ids): Collection;
+
+    public function all(): Collection;
+
+    public function when($condition, Closure $callback);
+}
---
+++ src/Contracts/Cacheable.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace KietNT\CachingModel\Contracts;
+
+interface Cacheable
+{
+    public static function primaryCacheKey(): string;
+
+    public static function getCacheKey($id, ?string $key = null): string;
+
+    public static function getCacheKeyList(): string;
+
+    public static function cacheTimeout(): int;
+
+    public function scopeCacheWithRelation($query);
+}
